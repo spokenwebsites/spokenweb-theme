414daeeabaa00102d86bfa1236eac33cf5adb9d4
maxwellstein <maxwell.stein@gmail.com>
2020-06-19 12:22:02 -0700

    thumbnails for author bios

--- wp-content/themes/spokenweb_2019/single-spokenweblog.php
+++ wp-content/themes/spokenweb_2019/single-spokenweblog.php
@@ -10,11 +10,11 @@ $cat_link = get_category_link( $cat_id );
 </div>
 
 <div class="px-3">
-  
+
 <?php
-  
+
   while (have_posts()) : the_post();
-      
+
   	$post_custom_fields = get_post_custom();
   	$date_end_orig = $post_custom_fields['date_end'][0];
   	$event_time = $post_custom_fields['event_time'][0];
@@ -28,41 +28,41 @@ $cat_link = get_category_link( $cat_id );
   	$author_image = $post_custom_fields['author_image'][0];
 
     $title = get_the_title();
-  
+
   	$audio = $post_custom_fields['audio'][0];
   	$subtitle = $post_custom_fields['subtitle'][0];
-    $audio_url = wp_get_attachment_url($audio);    
-  
+    $audio_url = wp_get_attachment_url($audio);
+
 ?>
 
 <div class="container featured-article mt-3 p-4">
-    
+
   <div class="row d-flex justify-content-between p-2">
     <div class="col-md-6 justify-content-center align-self-center">
       <h2 class="mb-3"><?php the_title();?><?php if(isset($subtitle) && $subtitle!="") echo " – $subtitle";?></h2>
 
-      
+
       <div class="mb-2 d-flex justify-content-end flex-row-reverse">
         <h5 class="mb-1"><?php the_time('F j, Y'); ?></h5>
         <h5 class="mb-1 mr-5"><?php echo $author;?></h5>
       </div>
-      
+
       <div class="col-md-5 col-sm-9 col-12 mb-2 justify-content-center align-self-center mx-0 px-0 d-md-none d-block">
-      
+
   			<?php  if ( has_post_thumbnail() ):?>
   				<?php $img_lg = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' ); $img_lg = $img_lg[0];?>
   				<img src="<?php echo $img_lg;?>" width="100%">
   			<?php endif;?>
       </div>
-      
+
       <div class="meta py-2">
         <p class="smaller"><?php if(has_category()!=0):?><?php the_category(', ');?><?php endif;?><?php if(has_tag()!=0):?><?php if(has_category()!=0):?> | <?php endif;?><?php the_tags('', ', ');?><?php endif;?></p>
       </div>
-      
+
       <div class="mt-2">
         <a class="twitter-share-button" href="https://twitter.com/intent/tweet" data-size="large">Tweet</a>
-        <?php $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";?>        
-      
+        <?php $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";?>
+
         <div class="fb-share-button mt-4" data-href="<?php echo $actual_link;?>" data-layout="button" data-size="large" data-mobile-iframe="true"></div>
       </div>
     </div>
@@ -79,11 +79,11 @@ $cat_link = get_category_link( $cat_id );
   <?php the_content();?>
 </div>
 
-    <?php $img_lg = wp_get_attachment_image_src($author_image, 'large' ); $img_lg_url = $img_lg[0];?>
+    <?php $img_lg = wp_get_attachment_image_src($author_image, 'thumbnail' ); $img_lg_url = $img_lg[0];?>
 
 <div class="container py-5 mb-5 px-0 authors">
   <div class="row d-flex justify-content-between p-2 flex-row-reverse">
-  
+
     <div class="col-sm-9 justify-content-center align-self-center">
       <h3 class="mb-3"><?php echo $author;?></h3>
       <div class="d-sm-none d-block w-50 mb-3">
@@ -91,9 +91,9 @@ $cat_link = get_category_link( $cat_id );
       </div>
       <?php echo wpautop($author_bio);?>
     </div>
-  
+
     <div class="col-sm-3 justify-content-center align-self-start mx-auto d-none d-sm-block">
-      <img class="w-100 rounded-circle" src="<?php echo $img_lg_url;?>">      
+      <img class="w-100 rounded-circle" src="<?php echo $img_lg_url;?>">
     </div>
   </div>
 
