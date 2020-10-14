4e5a640ed553ffc9638e695c49b2bb8c391e28b9
maxwellstein <maxwell.stein@gmail.com>
2020-10-13 20:25:52 -0700

    updated page-episodes

--- wp-content/themes/spokenweb_2019/page-episodes.php
+++ wp-content/themes/spokenweb_2019/page-episodes.php
@@ -5,15 +5,19 @@ $post_custom_fields = get_post_custom();
 $top_header = $post_custom_fields['top_header'][0];
 $left_column = $post_custom_fields['left_column'][0];
 $right_column = $post_custom_fields['right_column'][0];
+$bottom_section = $post_custom_fields['bottom_section'][0];
+$bottom_section_title = $post_custom_fields['bottom_title'][0];
+$bottom_section_left = $post_custom_fields['bottom_section_left'][0];
+$bottom_section_right = $post_custom_fields['bottom_section_right'][0];
 ?>
-<section class="container">
+<section class="container episodes-container">
   <div class="row">
     <div class="col-sm-12">
       <div class="text-center logo-container"><img src="<?php bloginfo('template_directory'); ?>/_/img/podcast_logo.png" width="108"/></div>
-      <h1>About the</h1>
-      <div class="accent"><h1>SpokenWeb Podcast</h1></div>      
+      <h1>Browse the</h1>
+      <div class="accent"><h1>Podcast Episodes</h1></div>
     </div>
-    <div class="col-md-10 offset-md-1 col-sm-12">
+    <div class="col-sm-12 text-center">
       <div class="subscribe-link-container"><a id="subscribe-link" href="https://the-spokenweb-podcast.simplecast.com/" target="_blank"><button class="btn btn-sw"><span class="oi oi-rss-alt"></span>SUBSCRIBE</button></a></div>
       <div class="header-container">
         <?php echo wpautop($top_header);?>
@@ -41,26 +45,25 @@ $right_column = $post_custom_fields['right_column'][0];
 <section class="container-fluid featured-episode">
   <div class="row">
     <div class="col-md-4 offset-md-1 col-sm-6 text-container">
-      
+
       <?php
       $args = array(
         'posts_per_page'=>1,
   			'post_type'=>'podcast',
-  			'post_status' => 'publish',
     		'order'=>'ASC',
     		'orderby'=>'menu_order'
   		);
-    
+
     	$featured_podcast_query = new WP_Query( $args );
       ?>
-    
+
     	<?php if ($featured_podcast_query->have_posts()) : $i=0; while ($featured_podcast_query->have_posts()) : $featured_podcast_query->the_post(); ?>
       <?php
 				$post_custom_fields = get_post_custom();
     		$subtitle = $post_custom_fields['subtitle'][0];
     		$producer = $post_custom_fields['producer'][0];
       ?>
-      
+
 			<?php if ( has_post_thumbnail()){
         $img_lg = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' ); $img_lg_url = $img_lg[0];
         $img_thumb = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' ); $img_thumb = $img_thumb[0];
@@ -72,21 +75,21 @@ $right_column = $post_custom_fields['right_column'][0];
         $img_lg_height = "";
         $img_thumb = "";
       } ?>
-      
-      
+
+
 
       <h3>Featured Episode</h3>
-      
+
       <h5><?php echo $subtitle;?></h5>
-      
+
       <h2><?php the_title();?></h2>
-      
+
       <p><?php the_excerpt();?></p>
-      
+
       <p><strong>Produced by <?php echo $producer;?></strong></p>
-            
+
       <?php endwhile; endif;?>
-      
+
     </div>
     <div class="col-md-6 offset-md-1 col-sm-6 img-container">
       <a href="<?php the_permalink();?>">
@@ -100,25 +103,43 @@ $right_column = $post_custom_fields['right_column'][0];
 
 <section class="container episodes">
   <div class="row">
-    
-    <div class="col-sm-12 text-center category-select-container">
-      <span class="title"><strong>SORT EPISODES BY:</strong> </span>
-      <div class="category-select" style="display:inline-block; margin-left:30px;">
+    <div class="col-sm-12 col-md-10 offset-md-1 text-left category-select-container">
+      <h4 class="title mb-3"><strong>SORT EPISODES BY:</strong></h4>
+      <div class="category-select filter float-md-left mb-3">
+        <a href="#all"><button class="btn btn-sw active">ALL</button></a>
+        <a href="#spokenweb-podcast"><button class="btn btn-sw">SPOKENWEB PODCAST</button></a>
+        <a href="#shortcuts"><button class="btn btn-sw">SHORTCUTS</button></a>
+      </div>
+      <div class="category-select sort float-md-right mb-5">
         <a href="#desc"><button class="btn btn-sw active">DATE (DESC)</button></a>
         <a href="#asc"><button class="btn btn-sw">DATE (ASC)</button></a>
       </div>
+      <div class="clearfix"></div>
     </div>
-  
+
+    <!-- <div class="col-sm-12 col-md-10 offset-md-1 text-center category-select-container">
+      <div class="category-select sort mb-5">
+        <div class="d-inline">
+          <strong class="mr-3">SORT EPISODES BY:</strong>
+          <a href="#desc"><button class="btn btn-sw active">DATE (DESC)</button></a>
+          <a href="#asc"><button class="btn btn-sw">DATE (ASC)</button></a>
+        </div>
+      </div>
+    </div> -->
+
     <div class="col-sm-12 col-md-10 offset-md-1">
-    
+
       <div class="desc row">
         <?php
         $args = array(
           'posts_per_page'=>-1,
     			'post_type'=>'podcast',
-    			'post_status' => 'publish',
+    			'post_status' => array('publish, private'),
+          // 'post_status' => 'publish',
       		'order'=>'DESC',
-          'orderby'=>'date'
+          'orderby'=>'date',
+          'meta_key'=>'type',
+          'meta_value'=>array('SpokenWeb Podcast', 'ShortCuts')
     		);
 
       	$podcast_query = new WP_Query( $args );
@@ -129,8 +150,9 @@ $right_column = $post_custom_fields['right_column'][0];
     			$post_custom_fields = get_post_custom();
       		$subtitle = $post_custom_fields['subtitle'][0];
       		$producer = $post_custom_fields['producer'][0];
+          $type = strtolower(str_replace(" ", "_", $post_custom_fields['type'][0]));
         ?>
-  
+
     		<?php if ( has_post_thumbnail()){
           $img_lg = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' ); $img_lg_url = $img_lg[0];
           $img_thumb = wp_get_attachment_image_src( get_post_thumbnail_id(), 'thumbnail' ); $img_thumb = $img_thumb[0];
@@ -142,35 +164,42 @@ $right_column = $post_custom_fields['right_column'][0];
           $img_lg_height = "";
           $img_thumb = "";
         } ?>
-  
-  
-        <div class="col-sm-4 col-6">
+
+
+        <div class="col-sm-4 col-6" data-type="<?php echo $type;?>">
           <div style="position:relative;">
           <h4 style="position:absolute; z-index:1000; bottom:37px; left:15px; font-size:16px;"><?php echo $subtitle;?></h4>
           <a href="<?php the_permalink();?>">
             <img class="event-img filter" src="<?php echo $img_thumb;?>" width="100%">
+            <?php if($type == 'shortcuts'):?>
+            <section class="event-red"></section>
+            <?php else:?>
             <section class="event-orange"></section>
+            <?php endif;?>
             <span class="listen-now"><span class="oi oi-media-play"></span>Listen Now</span>
           </a>
         </div>
-    
-    
+
+
           <h5><?php echo get_the_date();?></h5>
           <h4><a href="<?php the_permalink();?>"><?php the_title();?></a></h4>
           <?php the_excerpt();?>
         </div>
-  
+
         <?php endwhile; endif;?>
       </div>
-    
+
       <div class="asc row" style="display:none;">
         <?php
         $args = array(
           'posts_per_page'=>-1,
     			'post_type'=>'podcast',
-    			'post_status' => 'publish',
+    			'post_status' => array('publish, private'),
+          // 'post_status' => 'publish',
       		'order'=>'ASC',
-          'orderby'=>'date'
+          'orderby'=>'date',
+          'meta_key'=>'type',
+          'meta_value'=>array('SpokenWeb Podcast', 'ShortCuts')
     		);
 
       	$podcast_query = new WP_Query( $args );
@@ -180,9 +209,10 @@ $right_column = $post_custom_fields['right_column'][0];
         <?php
     			$post_custom_fields = get_post_custom();
       		$subtitle = $post_custom_fields['subtitle'][0];
-      		$producer = $post_custom_fields['producer'][0];
+          $producer = $post_custom_fields['producer'][0];
+          $type = strtolower(str_replace(" ", "_", $post_custom_fields['type'][0]));
         ?>
-  
+
     		<?php if ( has_post_thumbnail()){
           $img_lg = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' ); $img_lg_url = $img_lg[0];
           $img_thumb = wp_get_attachment_image_src( get_post_thumbnail_id(), 'thumbnail' ); $img_thumb = $img_thumb[0];
@@ -194,26 +224,60 @@ $right_column = $post_custom_fields['right_column'][0];
           $img_lg_height = "";
           $img_thumb = "";
         } ?>
-  
-  
-        <div class="col-sm-4 col-6">
+
+        <div class="col-sm-4 col-6" data-type="<?php echo $type;?>">
           <div style="position:relative;">
           <h4 style="position:absolute; z-index:1000; top:15px; left:15px; font-size:16px;"><?php echo $subtitle;?></h4>
           <a href="<?php the_permalink();?>">
             <img class="event-img filter" src="<?php echo $img_thumb;?>" width="100%">
+            <?php if($type == 'shortcuts'):?>
+            <section class="event-red"></section>
+            <?php else:?>
             <section class="event-orange"></section>
+            <?php endif;?>
             <span class="listen-now"><span class="oi oi-media-play"></span>Listen Now</span>
           </a>
-        </div>    
-    
+        </div>
+
           <h5><?php echo get_the_date();?></h5>
           <h4><a href="<?php the_permalink();?>"><?php the_title();?></a></h4>
           <?php the_excerpt();?>
         </div>
-  
+
         <?php endwhile; endif;?>
       </div>
     </div>
   </div>
 </section>
+
+<?php if (isset($bottom_section) && $bottom_section == true):?>
+<section class="container" style="padding-top: 0px; margin-top: -2rem;">
+  <div class="row pb-5">
+    <div class="col-md-10 offset-md-1 col-sm-12">
+      <?php if (isset($bottom_section_title) && $bottom_section_title!==""):?>
+      <div class="header-container text-center">
+        <h3><?php echo $bottom_section_title;?></h3>
+      </div>
+      <?php endif;?>
+      <div class="row">
+        <div class="col-md-10 offset-md-1 col-sm-12">
+          <hr>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-11 col-sm-12">
+      <div class="row entry-content">
+        <div class="col-md-5 offset-md-1 col-sm-6">
+          <?php echo wpautop($bottom_section_left);?>
+        </div>
+        <div class="col-md-5 offset-md-1 col-sm-6">
+          <?php echo wpautop($bottom_section_right);?>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?php endif;?>
+
 <?php get_footer(); ?>
