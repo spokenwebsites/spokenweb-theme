84378566eb521d21c3a2d559256c7d6909230475
maxwellstein <maxwell.stein@gmail.com>
2021-10-05 23:05:24 -0700

    adds staging title

--- header.php
+++ header.php
@@ -72,7 +72,8 @@
 </head>
 
 <body <?php body_class("$header_class noscroll"); ?>>
-
+  <?php $subdomain = array_shift((explode('.', $_SERVER['HTTP_HOST'])));?>
+  <?php if($subdomain == 'staging'):?><p>STAGING</p><?php endif;?>
   <header id="header">
 
     <nav class="navbar navbar-expand-lg navbar-main">
