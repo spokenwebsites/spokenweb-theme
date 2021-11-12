6c5520291c3aa55dd86f20203d21bfbbc3e66aa9
sukeshgandham <sukesh.gandham@gmail.com>
2021-11-12 17:36:26 -0500

    Updated the logo and added it's relative css

--- page-collections.php
+++ page-collections.php
@@ -1,3 +1,45 @@
+<style>
+ 
+ .filterby{
+	width: 313.23px;
+	height: 180.77px;
+	background:#DF7F28;
+ }
+ .collections_text{
+	transform: rotate(-1deg);
+	 background-color: #EC8E43; 
+	 display:inline-block;
+ }
+
+ .collections_pg{
+	font-family: Futura; 
+	font-style: normal;
+	font-weight: bold;
+	font-size: 19px;
+	line-height: 27px;
+	color: #312E2D;
+	padding-left: 40px;
+ }
+ .collections_logo{
+	width:670px; 
+	height: 409px; 
+	margin-left: 150px;
+ }
+
+ .filterby h5{
+	margin-left:20px;
+	 padding-top:17px
+ }
+
+ .filterby select{
+	border-radius: 12px!important; 
+	border: 1px solid #ced4da; 
+	margin-left:15px;
+	 width:90%
+ }
+ 
+</style>
+
 <?php get_header(); ?>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
@@ -11,20 +53,20 @@
 			<?php $query = new WP_Query($args); ?>
 			<?php if ($query->have_posts()) : $i = 0;
 				while ($query->have_posts()) : $query->the_post(); ?>
-					<div class="row">
+					<!-- <div class="row">
 						<div class="col-sm-3">
 							<h5 class="title"><?php the_title(); ?></h5>
 						</div>
-					</div>
+					</div> -->
 			<?php endwhile;
 			endif; ?>
 
 		<?php else : ?>
-			<div class="row">
+			<!-- <div class="row">
 				<div class="col-sm-3">
 					<h4 class="title"><?php the_title(); ?></h4>
 				</div>
-			</div>
+			</div> -->
 		<?php endif; ?>
 
 		<hr>
@@ -32,7 +74,7 @@
 
 		<div class="row inner-content">
 
-			<aside class="col-sm-3">
+			<!-- <aside class="col-sm-3">
 				<?php if (has_children()) : ?>
 					<?php $children = new WP_Query(array('post_type' => 'page', 'post_parent' => $post->ID, 'order' => 'ASC', 'orderby' => 'menu_order')); ?>
 					<?php if ($children->have_posts()) : while ($children->have_posts()) : $children->the_post(); ?>
@@ -43,20 +85,32 @@
 				<?php endif; ?>
 
 
-			</aside>
+			</aside> -->
 
-			<div class="col-sm-9">
-
-				<?php the_content(); ?>
+			<div class="col-sm-12 jumbotron" style="background: #F0F0F0;">
+				<div class="row">
+					<div class="col-sm-3">
+						<div class="collections_text">
+							<h1>Collections</h1>
+						</div>
+						<p class="collections_pg">
+						<b>	<?php the_content(); ?></b>
+						<ul style="border: 1px solid #000000; width:180px "></ul>
+						</p>
+						
+					</div>
+					<div class="col-sm-6">
+						<img class="collections_logo" src="<?php bloginfo('template_directory'); ?>/_/img/collections_logo.png" />
+					</div>
+				</div>
 			</div>
 
+			
 			<div class="col-sm-3">
 				<hr>
-
-				<h5>Filter by</h5>
-
-				<div class="form-group">
-					<label for="selectInstitution" class="small"><strong>Institution</strong></label>
+				<div class="form-group filterby "  >
+					<div><h5>Filter by</h5></div>
+					<label for="selectInstitution" class="small" style="margin-left:20px;"><strong>Institution</strong></label>
 					<select class="form-control" id="selectInstitution">
 						<option selected>All</option>
 					</select>
@@ -87,7 +141,8 @@
 							$subtype = $post_custom_fields['subtype'][0];
 							$collection_url = $post_custom_fields['url'][0];
 							?>
-							<?php// if(get_the_content()!=""):?>
+							
+							
 							<div class="col-3 people-container<?php if ($i % 4 == 0) echo " first"; ?>" data-i="<?php echo $i; ?>" data-institution="<?php echo $institution; ?>">
 								<div>
 									<a href="#<?php echo $post->post_name; ?>">
